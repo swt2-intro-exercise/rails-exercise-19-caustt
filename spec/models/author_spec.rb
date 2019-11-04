59baac89110a3bc8aa9fb9f6c4c1bd7d4b92b658
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2019-11-04 12:18:51 +0100

    Created failing new author Model Tests

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'New author', type: :model do
+
+	before :each do
+
+		@firt_name = 'Alan'
+		@last_name = 'Turing'
+		@homepage = 'http://wikipedia.de/Alan_Turing'
+
+		@author = Author.new first_name last_name homepage
+
+	end
+
+	it 'should have a first_name, last_name and homepage, which are all strings.' do
+
+		expect(@author.first_name).to eq(@first_name)
+		expect(@author.last_name). to eq(@last_name)
+		expect(@author.homepage).to eq(@homepage)
+
+	end
+
+	it 'should have a method name which returns the full name of an author' do
+
+		expect(@author.name).to eq(@first_name + @last_name)
+
+	end
+	
+end
