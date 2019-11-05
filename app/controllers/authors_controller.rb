23e47d4f3ebf7214c62e80c149e98951f7875f50
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2019-11-05 21:08:40 +0100

    Added new author creation

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,4 +3,14 @@ class AuthorsController < ApplicationController
   end
   def new
   end
+  def create
+  	@author = Author.new(post_params)
+
+  	@author.save
+  	redirect_to root_path, notice: 'Success!'
+  end
+
+  private def post_params
+  	params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
 end
