d43566358dd09b022b639eb0f68d04f02cf5cad1
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2019-11-06 16:19:41 +0100

    Implemented #11

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -16,9 +16,24 @@ class AuthorsController < ApplicationController
   	end
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def show
   	@author = Author.find(params[:id])
   end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if(@author.update(post_params))
+      redirect_to @author 
+    else
+      render :new
+    end
+  end
+
 
   private def post_params
   	params.require(:author).permit(:first_name, :last_name, :homepage)
