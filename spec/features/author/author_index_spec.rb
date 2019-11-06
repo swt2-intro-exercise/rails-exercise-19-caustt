949b347a55c68a9c42a68f4d336975214f988b7b
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2019-11-06 15:24:29 +0100

    Failing Test for #11

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -2,15 +2,25 @@ require 'rails_helper'
 
 describe "Author Index Page", type: :feature do
 
-	it "should show render" do
+	before :each do
+		@author = create(:author)
     visit authors_path
   end
 
+
+	it "should show render" do
+    expect(page).to have_text(@author.first_name)
+  end
+
   it "should link to the author" do
-  @author = create(:author)
-  visit authors_path
-  expect(page).to have_link @author.first_name, href: author_path(@author)
+  	expect(page).to have_link @author.first_name, href: author_path(@author)
 	end
+
+	it "should have a link to add a new author" do
+		expect(page).to have_link 'Add Author', href: new_author_path
+	end
+
+
 
 
 
