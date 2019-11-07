6e2feadf1e213c631bda67db900eee1b5018018f
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2019-11-07 10:31:39 +0100

    Failing Test for #16

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'New Paper', type: :model do
+
+  it "creation should fail withour a title" do
+  	@paper = Paper.new(venue: "Mind 49: 433-460", year: 1950)
+    expect(@paper).to_not be_valid
+  end
 end
