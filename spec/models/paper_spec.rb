96ed19df8de61734a1eec3c577879ba166ccd22f
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2019-11-07 10:48:28 +0100

    Failing Test for #17

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 describe 'New Paper', type: :model do
 
-  it "creation should fail withour a title" do
+  it "creation should fail without a title" do
   	@paper = Paper.new(venue: "Mind 49: 433-460", year: 1950)
     expect(@paper).to_not be_valid
   end
+
+  it "should have an empty list of authors" do
+  	@paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+  	expect(@paper.authors).to be_empty
+
+
+
+  end
+
 end
